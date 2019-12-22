ccc58f5990027813600cedd8a3dff386c9b5db71
Edward Stoner <edwardlstoner@gmail.com>
2019-12-22 13:44:41 -0700

    Pass tests

--- golf-course.js
+++ golf-course.js
@@ -4,6 +4,26 @@ class GolfCourse {
     this.difficulty = difficulty;
     this.openings = openings;
     this.features = features;
+    this.currentlyPlaying = [];
+  }
+
+  checkInGroup(group) {
+    if (group.length <= this.openings) {
+      this.openings -= group.length;
+      for (var i = 0; i < group.length; i++) {
+        this.currentlyPlaying.unshift(group[i].name);
+      }
+    }
+  }
+
+  recommendTees(golfer) {
+    if (golfer.handicap <= 10) {
+      return `You should play from the Difficult Tees.`
+    } else if (golfer.handicap <= 14 && golfer.handicap >= 11) {
+      return `You should play from the Middle Tees.`
+    } else if (golfer.handicap >= 15) {
+      return `You should play from the Forward Tees.`
+    }
   }
 }
 
---
+++ golfer.js
@@ -0,0 +1,46 @@
+class Golfer {
+  constructor(golferDetails) {
+    this.name = golferDetails.name;
+    this.handicap = golferDetails.handicap;
+    this.frustration = 0;
+    this.confidence = 0;
+  }
+
+  calculateAvg() {
+    return `I usually shoot a ${72 + this.handicap} on average.`
+  }
+
+  playRound(course) {
+    if (course.difficulty === 'hard') {
+      this.frustration = 500;
+    } else if (course.difficulty === 'moderate') {
+      this.frustration = 100;
+    }
+  }
+
+  hitTheRange() {
+    this.confidence += 10;
+  }
+
+  marvel(course) {
+    return `I love the ${course.features[0]} and ${course.features[1]} on this course!`;
+  }
+
+  whatYaShoot(score) {
+    if (score > 0) {
+      this.frustration += 20;
+      return 'Doh!';
+    } else if (score === 0) {
+      this.frustration -= 10;
+      return 'Booyah!';
+    } else if (score === -1) {
+      this.frustration -= 20;
+      return 'I AM THE GREATEST GOLFER ALIVE!';
+    } else if (score < -1) {
+      this.frustration -= 30;
+      return 'I AM THE GREATEST GOLFER ALIVE!';
+    }
+  }
+}
+
+module.exports = Golfer;
